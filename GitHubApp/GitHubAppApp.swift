e3e86a5af1021e2a16762c6873586c4c49d6dc7e
ThibaultGdf <thibaultgodef81@gmail.com>
2022-09-27 10:29:33 +0200

    Initial Commit

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Thibault GODEFROY on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
